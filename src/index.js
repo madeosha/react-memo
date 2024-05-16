8b32928e31d7abdc09a628e5f95555b8f37824a4
madeosha <lojkina95@yandex.ru>
2024-05-16 16:53:12 +0500

    eslint ошибки

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { SimpleModeProvider } from "./context/SimpleModeContext";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <SimpleModeProvider>
       <RouterProvider router={router}></RouterProvider>
-      </SimpleModeProvider>
+    </SimpleModeProvider>
   </React.StrictMode>,
 );
